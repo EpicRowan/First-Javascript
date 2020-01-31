00a359e1f2360d7e5298d4d3afa2bb76ec193f81
Your Name <you@example.com>
2020-01-31 13:00:02 -0800

    finished functions part of the lab

--- functions.js
+++ functions.js
@@ -4,13 +4,45 @@
 // 1. isHometown
 
 // Define your function here
-
+function isHometown(town) {
+  if (town === 'San Francisco')  {
+    return true;
+  }
+  return false;
+}
 
 // 2. getFullName
 
 // Define your function here
 
+// function getFullName(firstName, lastName) {
+//   return(`${firstName} ${lastName}`);
+// }
+
+const getFullName = (firstName, lastName) => (`${firstName} ${lastName}`);
+
+
 
 // 3. calculateTotal
+function calculateTotal(basePrice, state, tax) {
+  const subtotal = (basePrice * (1 + tax));
+  let fee = 0;
+
+  if (state=== 'CA'){
+    fee = (0.03 * subtotal);
+
+  } else if (state === 'PA') {
+    fee = 2;
+
+  } else if (state === 'MA') {
+    } if (basePrice <= 100) {
+      fee = 1;
+   
+   } else {
+      fee = 3;
+
+  } return (subtotal + fee);
+
+}
 
 // Define your function here
